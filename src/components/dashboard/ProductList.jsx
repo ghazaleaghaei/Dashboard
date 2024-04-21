50ffe2e2e1756bc54002539e87c8c6f9a3c6bcc0
ghazale aghaei <ghazale.ag@gmail.com>
2024-04-21 22:52:11 +0330

    commited by script

--- src/components/dashboard/ProductList.jsx
+++ src/components/dashboard/ProductList.jsx
@@ -1,6 +1,8 @@
-import { Arrow, Trash } from "../Exports"
+import { Arrow, Trash, useFetchData } from "../Exports"
 
 function ProductList() {
+    const { data, isLoading, error } = useFetchData(" http://localhost:5000/products", "_page=2&_limit=2")
+    console.log(data)
     return (
         <div class="rounded-lg bg-white border shadow-md p-3 m-3">
             <div class="flex gap-4">
@@ -18,9 +20,31 @@ function ProductList() {
                     Action
                     <Arrow class="w-5 aspect-square fill-sky-500" />
                 </button>
-                <input />
-                <input />
+                <select name="resultCount" id="resultCount" class="bg-white border rounded-md px-2 py-0 outline-none text-gray-500 ms-auto">
+                    <option value="100">100</option>
+                    <option value="150">150</option>
+                    <option value="200">200</option>
+                </select>
+                <input placeholder="Search" type="text" class="outline-none rounded-md placeholder:text-gray-400 px-3 border max-w-48" />
             </div>
+            <table class="table-auto w-full">
+                <tr class="*:font-normal *:bg-gray-100 *:rounded-md *:px-2 *:border *:text-start">
+                    <th class="flex"><input type="checkbox" />Image</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Status</th>
+                    <th>Permissions</th>
+                    <th>Action</th>
+                </tr>
+                {data.map(item => <tr key={item.id}>
+                    <td class="w-5"><input type="checkbox" />Image</td>
+                    <td>{item.name}</td>
+                    <td>{item.price}</td>
+                    <td>{item.status}</td>
+                    <td>{item.permission}</td>
+                    <td>{item.action}</td>
+                </tr>)}
+            </table>
 
         </div>
     )
